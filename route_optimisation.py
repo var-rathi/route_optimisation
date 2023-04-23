52eb87fd5258f5604c4bf1041e064e2aa4bbf1cb
user_varun <varun@unorg.in>
2023-04-23 14:48:25 +0530

    Removed Errors Directly from the class implementation and matched the objective function set depot weight to 0 This is a fully functional solution

--- route_optimisation.py
+++ route_optimisation.py
@@ -17,8 +17,8 @@ class plan_route(distance_matrix):
         self.distance_time_parameters=params
     def create_vehicle_parameters(self):
         params={}
-        params['vehicles_number']=15 #number of vehicles
-        params['capacity']=[2500,2500,1200,1200,1200,1200,1200,2500,2500,1200,2500,2500,2500,2500,2500] # Vehicle Capacity
+        params['vehicles_number']=20 #number of vehicles
+        params['capacity']=[2500,2500,1200,1200,1200,1200,1200,2500,2500,1200,2500,2500,2500,2500,2500,2500,2500,1200,1200,1200] # Vehicle Capacity
         self.vehicle_parameters=params
     def create_routing_index_manager(self):
         # considering 0 as the depot we need to amend this
@@ -93,15 +93,20 @@ class plan_route(distance_matrix):
             vehicle_route[vehicle_id]['delivery_locations']=max(0,len(vehicle_route[vehicle_id]['route_index'])-2)
         return vehicle_route
     def optimize_route(self):
+        self.data.weight[0]=0 # Depot weight is being initialized to zero
         self.create_distance_time_parameters()
         self.create_vehicle_parameters()
         self.create_routing_index_manager()
         self.create_routing_model()
         self.transit_callback_index = self.routing.RegisterTransitCallback(self.time_callback)
+
         # Define cost of each arc.
         self.routing.SetArcCostEvaluatorOfAllVehicles(self.transit_callback_index)
+        self.transit_callback_index_2 = self.routing.RegisterTransitCallback(self.time_callback)
         #Weight capacity constraint
         self.weight_callback_index = self.routing.RegisterUnaryTransitCallback(self.weight_callback)
+        self.weight_callback_index_2 = self.routing.RegisterUnaryTransitCallback(self.weight_callback)
+        self.weight_callback_index_3 = self.routing.RegisterUnaryTransitCallback(self.weight_callback)
         # Add Distance constraint.
         dimension_name = 'Time'
         self.routing.AddDimension(
@@ -110,31 +115,52 @@ class plan_route(distance_matrix):
             int(self.distance_time_parameters['speed']*5*1000),  # vehicle maximum travel time 2.5 hours
             True,  # start cumul to zero
             dimension_name)
+        self.routing.AddDimension(
+            self.transit_callback_index_2,
+            0,  # no slack
+            int(self.distance_time_parameters['speed']*5*1000),  # vehicle maximum travel time 2.5 hours
+            True,  # start cumul to zero
+            'time_callback_index_2')
         self.routing.AddDimensionWithVehicleCapacity(
             self.weight_callback_index,
             0,  # null capacity slack
             np.array(self.vehicle_parameters['capacity'])*4,  # vehicle maximum capacities
             True,  # start cumul to zero
             'Weight')
+        self.routing.AddDimensionWithVehicleCapacity(
+            self.weight_callback_index_2,
+            0,  # null capacity slack
+            np.array(self.vehicle_parameters['capacity'])*4,  # vehicle maximum capacities
+            True,  # start cumul to zero
+            'Weight_2')
+        self.routing.AddDimensionWithVehicleCapacity(
+            self.weight_callback_index_3,
+            0,  # null capacity slack
+            np.array(self.vehicle_parameters['capacity'])*4,  # vehicle maximum capacities
+            True,  # start cumul to zero
+            'Weight_3')
         # Minimizing net time travelled by all the vehicles also keeping time in limits
         time_dimension = self.routing.GetDimensionOrDie(dimension_name)
+        time_dimension_2 = self.routing.GetDimensionOrDie('time_callback_index_2')
         weight_dimension=self.routing.GetDimensionOrDie('Weight')
-        time_dimension.SetGlobalSpanCostCoefficient(1)
+        weight_dimension_2 = self.routing.GetDimensionOrDie('Weight_2')
+        weight_dimension_3 = self.routing.GetDimensionOrDie('Weight_3')
+        # time_dimension.SetGlobalSpanCostCoefficient(1)
         for vehicle_id in range(self.vehicle_parameters['vehicles_number']):
             index = self.routing.End(vehicle_id)
-            time_dimension.SetCumulVarSoftUpperBound(index,int(self.distance_time_parameters['speed']*2*1000), 1000)
+            time_dimension_2.SetCumulVarSoftUpperBound(index,int(self.distance_time_parameters['speed']*2*1000), 4000)
             time_dimension.SetCumulVarSoftUpperBound(index, int(self.distance_time_parameters['speed'] * 3.5 * 1000), 100000)
             weight_dimension.SetCumulVarSoftUpperBound(index,int(self.vehicle_parameters['capacity'][vehicle_id] ), 500)
-            weight_dimension.SetCumulVarSoftUpperBound(index, int(self.vehicle_parameters['capacity'][vehicle_id]*1.3), 5000)
-            weight_dimension.SetCumulVarSoftUpperBound(index,int(self.vehicle_parameters['capacity'][vehicle_id] * 2), 500000)
+            weight_dimension_2.SetCumulVarSoftUpperBound(index, int(self.vehicle_parameters['capacity'][vehicle_id]*1.3), 5000)
+            weight_dimension_3.SetCumulVarSoftUpperBound(index,int(self.vehicle_parameters['capacity'][vehicle_id] * 2), 500000)
         # Setting first solution heuristic.
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
         search_parameters.local_search_metaheuristic = (
             routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
-        search_parameters.solution_limit = 1
-        # search_parameters.time_limit.seconds = 90
+        # search_parameters.solution_limit = 1
+        search_parameters.time_limit.seconds = 180
 
         search_parameters.log_search = 1
         self.solution = self.routing.SolveWithParameters(search_parameters)
@@ -177,17 +203,14 @@ class plan_route(distance_matrix):
         for vehicle_id in range(self.vehicle_parameters['vehicles_number']):
             net_route_time += sol[vehicle_id]['route_time']
 
-            cost_2hrs += 4000 * max(0, sol[vehicle_id]['route_time'] - int(
-                self.distance_time_parameters['speed'] * 2 * 1000))
+            cost_2hrs += 4000 * max(0, sol[vehicle_id]['route_time'] - int(self.distance_time_parameters['speed'] * 2 * 1000))
             # print(max(0,sol[vehicle_id]['route_time']-int(self.distance_time_parameters['speed']*2*1000)))
             cost_3hrs += 100000 * max(0, sol[vehicle_id]['route_time'] - int(
                 self.distance_time_parameters['speed'] * 3.5 * 1000))
             costnormalweight += 500 * max(0, sol[vehicle_id]['weight'] - int(
                 self.vehicle_parameters['capacity'][vehicle_id]))
-            cost30weight += 5000 * max(0, sol[vehicle_id]['weight'] - int(
-                self.vehicle_parameters['capacity'][vehicle_id] * 1.3))
-            cost2weight += 500000 * max(0, sol[vehicle_id]['weight'] - int(
-                self.vehicle_parameters['capacity'][vehicle_id] * 2))
+            cost30weight += 5000 * max(0, sol[vehicle_id]['weight'] - int(self.vehicle_parameters['capacity'][vehicle_id] * 1.3))
+            cost2weight += 500000 * max(0, sol[vehicle_id]['weight'] - int(self.vehicle_parameters['capacity'][vehicle_id] * 2))
         print('net_route_time: {}'.format(net_route_time))
         print('cost_2hrs: {}'.format(cost_2hrs))
         print('cost_3hrs: {}'.format(cost_3hrs))
